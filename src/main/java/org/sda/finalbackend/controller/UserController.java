a2aeb5130f3d2a52c5c92d1cce365e04ab8edcfb
User <sebastian.groza15@gmail.com>
2024-02-07 20:17:00 +0200

    User CRUD done!

--- src/main/java/org/sda/finalbackend/controller/UserController.java
+++ src/main/java/org/sda/finalbackend/controller/UserController.java
@@ -1,15 +1,18 @@
 package org.sda.finalbackend.controller;
 
+import org.aspectj.lang.annotation.Around;
 import org.sda.finalbackend.entity.User;
 import org.sda.finalbackend.errors.InvalidEmailOrUsernameException;
+import org.sda.finalbackend.errors.UserNotFoundException;
 import org.sda.finalbackend.service.UserService;
 import org.sda.finalbackend.utils.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -41,6 +44,122 @@ public class UserController {
             return ResponseEntity.status(HttpStatusCode.valueOf(400)).body(response);
         }
     }
+    @PatchMapping("/user")
+public  ResponseEntity<ApiResponse> updateUser(@RequestBody User user){
+    try{
 
+        User userDb= this.userService.upedateUser(user);
+        ApiResponse response = new ApiResponse.Builder()
+                .status(200)
+                .message("User upedate  with succes")
+                .data(userDb)
+                .build();
+        return ResponseEntity.ok(response);
+
+
+    }catch (UserNotFoundException userNotFoundException){
+        ApiResponse response = new ApiResponse.Builder()
+                .status(400)
+                .message(userNotFoundException.getMessage())
+                .data(null)
+                .build();
+
+        return ResponseEntity.status(HttpStatusCode.valueOf(400)).body(response);
+    }
+    catch (InvalidEmailOrUsernameException invalidEmailOrUsernameException){
+        ApiResponse response = new ApiResponse.Builder()
+                .status(400)
+                .message(invalidEmailOrUsernameException.getMessage())
+                .data(null)
+                .build();
+
+        return ResponseEntity.status(HttpStatusCode.valueOf(400)).body(response);
+    }
+
+}
+
+@DeleteMapping("/user/{id}")
+public ResponseEntity<ApiResponse> deleteUser(@PathVariable("id") Integer id){
+        try{
+            userService.deleteUser(id);
+            ApiResponse response = new ApiResponse.Builder()
+                    .status(200)
+                    .message("User delete with success")
+                    .data(null)
+                    .build();
+            return  ResponseEntity.ok(response);
+
+        }catch (UserNotFoundException userNotFoundException){
+            ApiResponse response = new ApiResponse.Builder()
+                    .status(400)
+                    .message(userNotFoundException.getMessage())
+                    .data(null)
+                    .build();
+
+            return ResponseEntity.status(HttpStatusCode.valueOf(400)).body(response);
+
+        }
+}
+
+@GetMapping("/user")
+public ResponseEntity<ApiResponse> getAllUsers()
+{
+    List<User> users = userService.findAll();
+    ApiResponse response = new ApiResponse.Builder()
+            .status(200)
+            .message("User list")
+            .data(users)
+            .build();
+    return ResponseEntity.ok(response);
+}
+    @GetMapping("/user/username")
+    public ResponseEntity<ApiResponse> getByUsername(@RequestParam(name="username") String username) {
+
+        try {
+
+            User userDB = userService.findByUsername(username);
+            ApiResponse response = new ApiResponse.Builder()
+                    .status(200)
+                    .message("Found by username")
+                    .data(userDB)
+                    .build();
+
+            return ResponseEntity.ok(response);
+
+        } catch (UserNotFoundException e) {
+            ApiResponse response = new ApiResponse.Builder()
+                    .status(400)
+                    .message(e.getMessage())
+                    .data(null)
+                    .build();
+
+            return ResponseEntity.status(HttpStatusCode.valueOf(400)).body(response);
+        }
+
+    }
+    @GetMapping("/user/email")
+    public ResponseEntity<ApiResponse> getByEmail(@RequestParam(name="email") String email){
+
+        try {
+
+            User userDB = userService.findByEmail(email);
+            ApiResponse response = new ApiResponse.Builder()
+                    .status(200)
+                    .message("Found by uemail")
+                    .data(userDB)
+                    .build();
+
+            return ResponseEntity.ok(response);
+
+        } catch (UserNotFoundException e) {
+            ApiResponse response = new ApiResponse.Builder()
+                    .status(400)
+                    .message(e.getMessage())
+                    .data(null)
+                    .build();
+
+            return ResponseEntity.status(HttpStatusCode.valueOf(400)).body(response);
+        }
+    }
 
 }
--- src/main/java/org/sda/finalbackend/repository/UserRepository.java
+++ src/main/java/org/sda/finalbackend/repository/UserRepository.java
@@ -2,8 +2,10 @@ package org.sda.finalbackend.repository;
 
 import org.sda.finalbackend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,6 +16,9 @@ public interface UserRepository extends JpaRepository <User,Integer>{
     Optional<User> getByUsername(String username);
 
     Optional<User>getByEmailOrUsername(String email,String username);
+    @Query(value = "SELECT * FROM tbl_users", nativeQuery = true)
+    List<User> selectAllUsers();
+
 
 
 }
